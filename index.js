a5485249d1a7997c1dbc3fd681b566bc00f66df2
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-04 20:12:43 +0700

    Lesson 2

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
-import http from "http";
+import express from "express";
 import { datas } from "./data.js";
-console.log(datas);
-
+const app = express();
 const PORT = 8000;
-const app = http.createServer((request, response) => {
-  /**
-   * Tuyến đường chính http://localhost:8000/ -> /
-   * Tuyến đường nhỏ đi ra từ tuyến chính -> /danh-cho-nam
-   */
 
-  const endpoint = request.url;
-  switch (endpoint) {
-    // với base endpoint (base API), mặc định base endpoint sẽ là /
-    case "/":
-      response.end(`<h1>Home page<h1>`);
-      break;
-    // với endpoint /students
-    case "/san-pham":
-      response.end(`<h1>Shop</h1>`);
-      break;
+app.use(express.json()); // Middleware
 
-    case "/products":
-      response.end(JSON.stringify(datas));
-      break;
-    default:
-      response.end(`Error, Notfound API!`);
-      break;
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/san-pham", (req, res) => {
+  console.log(req.query);
+  const brand = req.query.brand;
+  if (brand) {
+    const filterData = datas.filter((item) => item.brand === brand);
+    res.send(filterData);
+  }
+  res.send(datas);
+});
+
+app.get("/san-pham/:id", (req, res) => {
+  // :id là 1 param trong object req.params
+  const id = req.params.id;
+  if (id) {
+    const product = datas.find((item) => item.id === +id);
+    if (!product) {
+      res.status(404).send("Không tìm thấy sản phẩm");
+      return;
+    }
+    res.send(product);
   }
 });
 
-// CRUD = Create - Read - Update - Delete
+// ! Path bao gồm 2 thành phần chính (1 là cố định, 2 là biến)
+// ! Callback là hàm được truyền vào một hàm khác như một tham số
+
+app.post("/signup", (req, res) => {
+  const user = req.body;
+  if (user.password.length < 6) {
+    res.status(400).send("Mật khẩu phải có ít nhất 6 ký tự");
+    return;
+  }
+  if (user.usename === "hoangnm") {
+    res.status(400).send("Tên đăng nhập đã tồn tại");
+    return;
+  }
+  //! Sau khi validation data, tạo user mới và lưu vào database
+  res.send("Đăng ký thành công");
+});
+
+app.post("/login", (req, res) => {
+  res.send("Đăng nhập thành công");
+});
+
+app.put("/reset-password", (req, res) => {
+  res.send("Reset password thành công");
+});
+
+app.delete("/delete-account", (req, res) => {
+  res.send("Xóa tài khoản thành công");
+});
+
+// ! GET PATCH PUT POST DELETE
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on PORT ${PORT}`);
 });
