bc5e9ca21ef910b3dea2c936e27b408f9c9e49fd
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-22 20:40:55 +0700

    Lesson 7: Check Permission

--- routes/product.js
+++ routes/product.js
@@ -7,11 +7,16 @@ import {
   softRemoveProductById,
   updateProduct,
 } from "../controllers/product.js";
+import { checkPermission } from "../middlewares/checkPermission.js";
 
 const productRouter = Router();
 
 productRouter.get("/", getProducts);
 productRouter.get("/:id", getProductById);
+
+// ! Check permission for method: DELETE, PUT, POST
+productRouter.use(checkPermission);
+
 productRouter.post("/", createProduct);
 productRouter.delete("/delete/:id", removeProductById);
 productRouter.put("/remove/:id", softRemoveProductById);
